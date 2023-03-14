d6cb088a97a6197cae42cfe8f962caa6f48da64d
LethalSnake1337 <49997647+LethalSnake1337@users.noreply.github.com>
2023-03-14 16:13:55 +0100

    Update main.rs
    Testing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 use colored::Colorize;
 #[tokio::main]
 async fn main() {
+<<<<<<< Updated upstream
     println!("{}", "Hello, world!".magenta());
+=======
+    println!("{}", "Hello, world!".red());
+>>>>>>> Stashed changes
 }
