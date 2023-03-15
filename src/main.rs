1e7db2149ef1918eeca9d7807113f4e2bfc9ca12
LethalSnake1337 <49997647+LethalSnake1337@users.noreply.github.com>
2023-03-15 16:37:52 +0100

    Added Help Command

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,45 @@
 use colored::Colorize;
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+const HELP_MESSAGE: &str = "
+    Bot Supports following commands:
+    !help
+    !stock
+    ... more stuff blah blah blah
+";
+
+const HELP_COMMAND: &str = "!help";
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == HELP_COMMAND {
+            if let Err(why) = msg.channel_id.say(&ctx.http, HELP_MESSAGE).await {
+                println!("{} {:?}", "Error sending message: ".red(), why);
+            }
+        }
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    println!("{}", "Hello, world!".magenta());
-    println!("{}", "Hello, world!".red());
+    let token = env::var("DISCORD_TOKEN").expect("Expted a token in the environment");
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .await
+        .expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
