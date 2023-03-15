fe88fe9fa94cbd709233f26133e37c497a142d33
tamemo99 <Tamemayash@gmail.com>
2023-03-15 15:35:12 +0100

    tested a help command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,41 @@
 use colored::Colorize;
+use std::env;
+
+use serenity::async_trait;
+use serenity::model::channel::Message;
+use serenity::prelude::*;
+
+
+const HELP_MESSAGE: &str = "
+    Bot supports the following commands:
+    !help 
+    !stock
+";
+
+const HELP_COMMAND: &str = "!help";
+
+struct Handler;
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg:Message){
+        if msg.content == HELP_COMMAND{
+            if let Err(why) = msg.channel_id.say(&ctx.http,HELP_MESSAGE).await {
+                println!("{} {:?}", "Error sending message: {:?}".red(),why);
+            }
+        }
+    }
+    
+}
+
 #[tokio::main]
 async fn main() {
+    let token: String = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
+    let intents = GatewayIntents::GUILD_MESSAGES 
+    | GatewayIntents::DIRECT_MESSAGES 
+    | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Err creating client");
+    if let Err(why) = client.start().await{
+        println!("Client error: {:?}", why);
+    }
     println!("{}", "Hello, world!".magenta());
 }
